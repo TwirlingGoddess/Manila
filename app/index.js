5260902869fe40bc320f600ab784de5bd16965de
Lee Graham <graham.la3@gmail.com>
2019-01-15 19:16:41 -0700

    Create User component and display in App

--- app/index.js
+++ app/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'
+import './index.css';
+import User from '../Components/User.js'
 
 class App extends Component {
   render() {
     return(
       <div>
         <h1>Hello World</h1>
+        <User />
       </div>
     )
   }
