1365b1800faf022194badd56d6c338b59c7780fe
Lee Graham <graham.la3@gmail.com>
2019-01-15 20:48:03 -0700

    Insert Bids & Asks tables

--- Components/Orders.js
+++ Components/Orders.js
@@ -15,10 +15,47 @@ class Orders extends Component {
       )
     })
 
+    var bids = orders.filter(order => order.type==='bid')
+
+    var asks = orders.filter(order => order.type === 'ask')
+
+    var bidsArray = bids.map(order => {
+      return(
+        <div>
+          <h3>{ order.id }</h3>
+          <h4>{ order.type }</h4>
+          <h4>{ order.price }</h4>
+          <h4>{ order.volume }</h4>
+        </div>
+      )
+    })    
+    var asksArray = asks.map(order => {
+      return(
+        <div>
+          <h3>{ order.id }</h3>
+          <h4>{ order.type }</h4>
+          <h4>{ order.price }</h4>
+          <h4>{ order.volume }</h4>
+        </div>
+      )
+    })
+
     return(
       <div>
-        <h1>Orders</h1>
-        { ordersArray }
+        <hr />
+          <h1>Bids</h1>
+          <article>
+            { bidsArray }
+          </article>
+          <h1>All Orders</h1>
+          <article>
+            { ordersArray }
+          </article>
+          <h1>Asks</h1>
+          <article>
+            { asksArray }
+          </article>
+
       </div>
     )
   }
